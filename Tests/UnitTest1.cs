b89c0edf01df70a86676f5fdacfe11bf28e0b06a
MikeRoot <mike.root@resmancloud.com>
2018-10-15 12:48:56 -0600

    Add Test project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Controllers;
+
+namespace Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        HomeController homeController;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            homeController = new HomeController();
+        }
+
+        [TestMethod]
+        public void Index()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void NewGame()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void Find()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void ShowAllMines()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
